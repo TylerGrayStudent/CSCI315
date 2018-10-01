45de548d67a81675554b47ee183d41eeee70e7a6
Tyler Gray <wymngray@gmail.com>
2018-10-01 12:51:41 -0400

    Add Proj 1

---
+++ proj1/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tyler
+ * Date: 9/26/18
+ * Time: 11:31 AM
+ */
+?>
+<?php date_default_timezone_set('America/New_York');?>
+<?php include 'view/header.php';?>
+
+<?php header("Location: product_display/index.php");?>
+
+<?php include "view/footer.php";?>
